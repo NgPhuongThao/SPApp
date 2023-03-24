8805b54edb8a2a67983644855eec78913953faf6
NgPhuongThao <nguyen.phuongthaof@gmail.com>
2023-03-24 10:00:09 +0100

    Création du menu

--- app/src/main/java/com/example/spapp/MainActivity.java
+++ app/src/main/java/com/example/spapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.spapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.my_menu, menu);
+        return true;
     }
 }
